e637b0e16a5fb24330f1479b4b688c43292dcde8
David Alexander <david.alexander01@ui.ac.id>
2023-05-30 11:17:52 +0700

    add url mapping to get user

--- auth_service/urls.py
+++ auth_service/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register, login, logout
+from .views import register, login, logout, get_user
 
 app_name = "auth_service"
 
 urlpatterns = [
     path("register", register, name="register"),
+    path("user", get_user, name="user"),
 ]
