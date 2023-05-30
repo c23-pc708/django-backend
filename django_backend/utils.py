e67eed3fad00c747d82f985bed0bd398847a648d
David Alexander <david.alexander01@ui.ac.id>
2023-05-30 10:01:31 +0700

    add documentation

--- django_backend/utils.py
+++ django_backend/utils.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 
 def validate_params(request, params):
-    """Validate URL query params"""
+    """Validate URL query params. If returns None, params are valid."""
     for param in params:
         res = request.query_params.get(param)
         if res is None:
@@ -14,7 +14,7 @@ def validate_params(request, params):
 
 
 def validate_body(request, attrs):
-    """Validate request body for POST requests"""
+    """Validate request body for POST requests. If returns None, params are valid."""
     for attr in attrs:
         res = request.data.get(attr)
         if res is None:
