834e51d3a5bc207761c4f2a3ca7f563ee79386f5
David Alexander <david.alexander01@ui.ac.id>
2023-06-07 11:53:09 +0700

    add mock destinations api

--- api_service/models.py
+++ api_service/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import json
 
 # Create your models here.
+
+
+class Destination(models.Model):
+    destination_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, default="")
+    # https://stackoverflow.com/questions/22340258/list-field-in-model
+    type = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    description = models.CharField(max_length=2000)
+
+    def set_type(self, types):
+        self.type = json.dumps(types)
+
+    def get_type(self):
+        return json.loads(self.type)
--- api_service/views.py
+++ api_service/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+import json
+
 
 # Create your views here.
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def destinations(request):
+    return Response(
+        data=[
+            {
+                "destinationId": 1,
+                "name": "destination one",
+                "type": ["food", "sightseeing"],
+                "location": "Kuta",
+                "description": "asdjfalskdj falskdjhf lkash jdlfkajsh dlfkajsh dlkfjah sdlkfjah sklf",
+            },
+            {
+                "destinationId": 2,
+                "name": "the second destination",
+                "type": ["food", "sightseeing"],
+                "location": "Pantai Kuta",
+                "description": "asdjfalskdj falskdjhf lkash jdlfkajsh dlfkajsh dlkfjah sdlkfjah sklf",
+            },
+        ]
+    )
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def destination_detail(request, destination_id):
+    return Response(
+        data={
+            "destinationId": 1,
+            "name": "destination one",
+            "type": ["food", "sightseeing"],
+            "location": "Kuta",
+            "description": "asdjfalskdj falskdjhf lkash jdlfkajsh dlfkajsh dlkfjah sdlkfjah sklf",
+        },
+    )
