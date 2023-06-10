51b39f9dfc8bb04529e0a159db5f0e91a8be042b
David Alexander <david.alexander01@ui.ac.id>
2023-06-10 22:02:43 +0700

    add filter based on type

--- api_service/views.py
+++ api_service/views.py
@@ -1,7 +1,9 @@
+from django.core import serializers
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from .models import Destination
 import json
 
 
@@ -9,26 +11,31 @@ import json
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def destinations(request):
-    return Response(
-        data=[
-            {
-                "destinationId": 1,
-                "name": "destination one",
-                "type": "Art",
-                "location": "Kuta",
-                "opening_type": "10.00-15.00",
-                "description": "asdjfalskdj falskdjhf lkash jdlfkajsh dlfkajsh dlkfjah sdlkfjah sklf",
-            },
-            {
-                "destinationId": 2,
-                "name": "the second destination",
-                "type": "Entertainment",
-                "location": "Pantai Kuta",
-                "opening_time": "10.00-15.00",
-                "description": "asdjfalskdj falskdjhf lkash jdlfkajsh dlfkajsh dlkfjah sdlkfjah sklf",
-            },
-        ]
-    )
+    print("test")
+    dest = Destination.objects.all()
+
+    art = request.query_params.get("art")
+    if art is not None:
+        dest = dest.filter(art=1)
+
+    entertainment = request.query_params.get("entertainment")
+    if entertainment is not None:
+        dest = dest.filter(entertainment=1)
+
+    sightings = request.query_params.get("sightings")
+    if sightings is not None:
+        dest = dest.filter(sightings=1)
+
+    culinary = request.query_params.get("culinary")
+    if culinary is not None:
+        dest = dest.filter(culinary=1)
+
+    shopping = request.query_params.get("shopping")
+    if shopping is not None:
+        dest = dest.filter(shopping=1)
+
+    dest_list = serializers.serialize("json", dest)
+    return Response(data=json.loads(dest_list))
 
 
 @api_view(["GET"])
