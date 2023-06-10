6a12f84399ad4a33163be34966307e621f65df62
David Alexander <70895129+ogiorgil@users.noreply.github.com>
2023-06-10 22:42:14 +0700

    Destination service (#7)
    * create destination model
    
    * add destination insert queries
    
    * add filter based on type
    
    * add search based on name
    
    * add endpoint to get one destination
    
    * only return specific fields on destination list
    
    * remove debug message
    
    * remove the need of serializer
    
    * add error handling
    
    * add migrate to build step
    
    * fix indexerror bug

--- api_service/models.py
+++ api_service/models.py
@@ -1,14 +1,39 @@
 from django.db import models
 import json
 
+
 # Create your models here.
-
-
 class Destination(models.Model):
-    destination_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, default="")
-    # https://stackoverflow.com/questions/22340258/list-field-in-model
-    type = models.CharField(max_length=200)
-    location = models.CharField(max_length=100)
-    opening_time = models.CharField(max_length=100)
-    description = models.CharField(max_length=2000)
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField(blank=True, null=True)
+    art = models.IntegerField(blank=True, null=True)
+    entertainment = models.IntegerField(blank=True, null=True)
+    sightings = models.IntegerField(blank=True, null=True)
+    culinary = models.IntegerField(blank=True, null=True)
+    shopping = models.IntegerField(blank=True, null=True)
+    image_link = models.TextField(blank=True, null=True)
+    location = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    weekdays_time = models.TextField(blank=True, null=True)
+    weekend_time = models.TextField(blank=True, null=True)
+    latitude = models.DecimalField(
+        max_digits=8, decimal_places=7, blank=True, null=True
+    )
+    longitude = models.DecimalField(
+        max_digits=10, decimal_places=7, blank=True, null=True
+    )
+    lowest_price = models.IntegerField(blank=True, null=True)
+    highest_price = models.IntegerField(blank=True, null=True)
+    additional_price = models.IntegerField(blank=True, null=True)
+    column_1_star = models.IntegerField(blank=True, null=True)
+    column_2_star = models.IntegerField(blank=True, null=True)
+    column_3_star = models.IntegerField(blank=True, null=True)
+    column_4_star = models.IntegerField(blank=True, null=True)
+    column_5_star = models.IntegerField(blank=True, null=True)
+    total_reviews = models.IntegerField(blank=True, null=True)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, blank=True, null=True)
+    location_link = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = "destination"
--- api_service/views.py
+++ api_service/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from .models import Destination
 import json
 
 
@@ -9,38 +10,59 @@ import json
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def destinations(request):
-    return Response(
-        data=[
-            {
-                "destinationId": 1,
-                "name": "destination one",
-                "type": "Art",
-                "location": "Kuta",
-                "opening_type": "10.00-15.00",
-                "description": "asdjfalskdj falskdjhf lkash jdlfkajsh dlfkajsh dlkfjah sdlkfjah sklf",
-            },
-            {
-                "destinationId": 2,
-                "name": "the second destination",
-                "type": "Entertainment",
-                "location": "Pantai Kuta",
-                "opening_time": "10.00-15.00",
-                "description": "asdjfalskdj falskdjhf lkash jdlfkajsh dlfkajsh dlkfjah sdlkfjah sklf",
-            },
-        ]
+    dest = Destination.objects.all()
+
+    search = request.query_params.get("q")
+    if search is not None:
+        dest = dest.filter(name__icontains=search)
+
+    art = request.query_params.get("art")
+    if art is not None:
+        dest = dest.filter(art=1)
+
+    entertainment = request.query_params.get("entertainment")
+    if entertainment is not None:
+        dest = dest.filter(entertainment=1)
+
+    sightings = request.query_params.get("sightings")
+    if sightings is not None:
+        dest = dest.filter(sightings=1)
+
+    culinary = request.query_params.get("culinary")
+    if culinary is not None:
+        dest = dest.filter(culinary=1)
+
+    shopping = request.query_params.get("shopping")
+    if shopping is not None:
+        dest = dest.filter(shopping=1)
+
+    dest = dest.values(
+        "id",
+        "name",
+        "art",
+        "entertainment",
+        "sightings",
+        "culinary",
+        "shopping",
+        "image_link",
+        "location",
+        "description",
+        "weekdays_time",
+        "weekend_time",
+        "lowest_price",
+        "highest_price",
+        "rating",
+        "location_link",
     )
+    dest_list = list(dest)
+    return Response(data=dest_list)
 
 
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def destination_detail(request, destination_id):
-    return Response(
-        data={
-            "destinationId": 1,
-            "name": "destination one",
-            "type": "Art",
-            "location": "Kuta",
-            "opening_time": "10.00-15.00",
-            "description": "asdjfalskdj falskdjhf lkash jdlfkajsh dlfkajsh dlkfjah sdlkfjah sklf",
-        },
-    )
+    try:
+        dest = Destination.objects.get(id=destination_id).values()
+        return Response(data=dest[0])
+    except Destination.DoesNotExist:
+        return Response(status=404, data={"message": "Destination not found."})
